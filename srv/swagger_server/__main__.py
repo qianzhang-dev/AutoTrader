8c9ff99caf9c299c42fcb87aa903504dcaa2dcab
Hanzhang Zeng <48038149+Hazhzeng@users.noreply.github.com>
2021-01-24 01:12:19 -0800

    Add swagger ui at /api-docs

--- srv/swagger_server/__main__.py
+++ srv/swagger_server/__main__.py
@@ -1,15 +1,22 @@
-#!/usr/bin/env python3
+# pylint: disable=import-error
+
+from os import path
 
 import connexion
 
 from swagger_server import encoder
+from swagger_ui import flask_api_doc
+
+
+SWAGGER_CONFIG = path.join(path.dirname(__file__), 'swagger', 'swagger.yaml')
 
 
 def main():
-    app = connexion.App(__name__, specification_dir='./swagger/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'AutoTrader'}, pythonic_params=True)
-    app.run(port=8080)
+    app_ = connexion.App(__name__, specification_dir='./swagger/')
+    app_.app.json_encoder = encoder.JSONEncoder
+    app_.add_api('swagger.yaml', arguments={'title': 'AutoTrader'}, pythonic_params=True)
+    flask_api_doc(app_.app, config_path=SWAGGER_CONFIG, url_prefix='/api-docs')
+    app_.run(port=3000)
 
 
 if __name__ == '__main__':
