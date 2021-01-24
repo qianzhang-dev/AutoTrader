e66b6ccb61edeabc0bb893b3c7531a4c7af10d6c
Hanzhang Zeng <48038149+Hazhzeng@users.noreply.github.com>
2021-01-23 23:44:01 -0800

    Adjust git ignore

--- SERVER/index.js
+++ SERVER/index.js
@@ -7,6 +7,7 @@ const launchServer = async () => {
     this.expressServer = new ExpressServer(config.URL_PORT, config.OPENAPI_YAML);
     this.expressServer.launch();
     logger.info('Express server running');
+    logger.info(`For OpenAPI reference documentation, please visit http://localhost:${config.URL_PORT}/api-docs`)
   } catch (error) {
     logger.error('Express Server failure', error.message);
     await this.close();
