d99c230967067780056dd4efcea70dd91b8f3a06
Hanzhang Zeng <48038149+Hazhzeng@users.noreply.github.com>
2021-01-26 01:53:18 -0800

    Implemented login authorization

--- srv/recover.ps1
+++ srv/recover.ps1
@@ -1,6 +1,10 @@
-git checkout .\.gitignore
-git checkout .\swagger_server\__init__.py
-git checkout .\swagger_server\__main__.py
+git checkout requirements.txt
+git checkout test`-requirements.txt
 
-git add .\swagger_server\swagger\swagger.yaml
-git add .\instruction.md
+git checkout ./.gitignore
+git checkout ./swagger_server/__init__.py
+git checkout ./swagger_server/__main__.py
+git checkout ./swagger_server/controllers/authorization_controller.py
+
+git add ./swagger_server/swagger/swagger.yaml
+git add ./instruction.md
